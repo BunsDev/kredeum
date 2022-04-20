e50da34138c6b1a0dd7453830fd89fbf02823363
zapaz.eth <alain@kredeum.com>
2022-04-20 10:41:27 +0200

    svelte init for swarm bee-js

---
+++ swarm/src/testbeejs.ts
@@ -0,0 +1,15 @@
+import { Bee } from "@ethersphere/bee-js";
+
+const bee = new Bee("http://localhost:1633");
+
+const testBeeJS = async () => {
+  console.log("testBeeJS");
+
+  const result = await bee.uploadData("a2fae7a4314c63436d2a575296afe35496bdaa12141c2b73e068c2d175c6ad7c", "Bee is awesome!");
+  console.log("data upload reference", result.reference);
+
+  const retrievedData = await bee.downloadData(result.reference);
+  console.log("I really hope that", retrievedData.text()); // prints 'Bee is awesome!'
+};
+
+export { testBeeJS };
